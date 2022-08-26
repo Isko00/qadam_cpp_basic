6f134026167750ddb80aed6a8ce0b760fef4f215
Anguis <Islam.ospanov00@gmail.com>
2022-08-26 17:45:45 +0600

    new commit.

--- structure/lecture/test.cpp
+++ structure/lecture/test.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 // создать структуру компьютер 
 // создать у этой структуры аттрибут вес = 10
 // создать метод типа инт, который возвращает нам вес
 // использовать структуру в функции мейн
-struct EnemySpaceShip {
-    int x_coordinate = 2;
-    int y_coordinate = 3;
-    int weapon_power;
+struct Computer {
+    int weight = 10;
     
-    int getNewEnemy() {
-        return 0;
+    int get_weight() {
+        return weight;
     }
 };
 
 int main() {
-    EnemySpaceShip a;
-    int number = a.getNewEnemy();
+    Computer my_computer;
+
+    cout << my_computer.get_weight();
+
+    return 0;
 }
